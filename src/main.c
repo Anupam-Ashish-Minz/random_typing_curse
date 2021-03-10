13262da6d4fd0ad61cfeaa2d3ac4920fa70d1c0d
Anupam Ashish Minz <anupamminz300am@gmail.com>
2021-03-10 14:49:41 +0530

    radom letters

--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ncurses.h>
+#include <time.h>
 
 int main() {
 
     char letters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    time_t t;
+    srand((unsigned)time(&t));
 
     initscr();
     cbreak();
@@ -12,8 +17,9 @@ int main() {
     refresh();
     
     for (int i=0; i<5; i++) {
+        char rand_char = letters[rand()%26];
+        printw("%c\n", rand_char);
         char c = getch();
-        printw("%c\n", c);
         refresh();
     }
 
